71c628a6e9b3081ee75e32fb85e80323d2292f99
Matheus Duarte <matheus.galvao@centralar.com.br>
2020-03-04 15:03:41 -0300

    Adicionando dialogs

--- src/views/TarefaList/TarefaList.js
+++ src/views/TarefaList/TarefaList.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
 
 import { TarefasToolbar, TarefasTable } from './components';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button
+} from '@material-ui/core';
 import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
@@ -17,6 +24,8 @@ const TarefaList = () => {
   const classes = useStyles();
 
   const [tarefas, setTarefas] = useState([]);
+  const [openDialog, setOpenDialog] = useState(false); 
+  const [mensagem, setMensagem] = useState('');
 
   const API_URL = 'https://minhastarefas-api.herokuapp.com/tarefas';
   const headers = { 'x-tenant-id' : 'matheusg223@hotmail.com' }
@@ -27,8 +36,11 @@ const TarefaList = () => {
     }).then( response => {
       const novaTarefa = response.data
       setTarefas( [...tarefas, novaTarefa] )
+      setMensagem('Item adicionado com sucesso')
+      setOpenDialog(true)
     }).catch( erro => {
-      console.log(erro)
+      setMensagem('Ocorreu um erro')
+      setOpenDialog(true)
     })
   }
 
@@ -40,7 +52,8 @@ const TarefaList = () => {
       console.log(listarTarefas)
       setTarefas(listarTarefas)
     }).catch( erro => {
-      console.log(erro)
+      setMensagem('Ocorreu um erro')
+      setOpenDialog(true)
     })
   }
 
@@ -55,8 +68,11 @@ const TarefaList = () => {
         }
       })
       setTarefas(lista)
+      setOpenDialog(true)
+      setMensagem('Iem atualizado com sucesso')
     }).catch( erro => {
-      console.log(erro)
+      setMensagem('Ocorreu um erro')
+      setOpenDialog(true)
     })
   }
 
@@ -66,9 +82,11 @@ const TarefaList = () => {
     }).then(response => {
       const lista = tarefas.filter( tarefa => tarefa.id !== id)
       setTarefas(lista)
-      console.log(response)
+      setOpenDialog(true)
+      setMensagem('Item removido com sucesso')
     }).catch( erro => {
-      console.log(erro)
+      setMensagem('Ocorreu um erro')
+      setOpenDialog(true)
     })
   }
 
@@ -84,6 +102,15 @@ const TarefaList = () => {
                       deleteAction={deletar}
                       tarefas={tarefas} />
       </div>
+      <Dialog open={openDialog} onClose={e => setOpenDialog(false)}>
+        <DialogTitle>Atenção</DialogTitle>
+        <DialogContent>
+          {mensagem}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={e => setOpenDialog(false)}>Fechar</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
